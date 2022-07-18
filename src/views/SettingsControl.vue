2c5c10fff97b41c4cd6b8a5b32f9169f7ff1843f
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2022-07-17 22:49:43 -0300

    Update SettingsControl.vue

--- src/views/SettingsControl.vue
+++ src/views/SettingsControl.vue
@@ -58,7 +58,7 @@ export default {
     }
   }
   &__btn:disabled &__icon {
-    fill: rgba(rgb(125, 125, 125), .1);
+    fill: #607D8B;
   }
   &__btn:disabled:hover {
     background-color: transparent;
